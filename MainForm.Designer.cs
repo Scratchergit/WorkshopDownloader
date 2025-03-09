f1e5f63974d1fdc464b18c6a0cb380cb0c59e67d
Scratchergit <scratchjupaax1@gmail.com>
2025-03-09 14:58:45 +0200

    SOmestugg

---
+++ MainForm.Designer.cs
@@ -0,0 +1,313 @@
+namespace ModDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer = new SplitContainer();
+            label1 = new Label();
+            lstGames = new ListBox();
+            label6 = new Label();
+            txtNewGameId = new TextBox();
+            label5 = new Label();
+            txtNewGameName = new TextBox();
+            btnAddGame = new Button();
+            label4 = new Label();
+            txtModIds = new TextBox();
+            label3 = new Label();
+            txtWorkshop = new TextBox();
+            btnBrowseSteamCMD = new Button();
+            txtSteamCMD = new TextBox();
+            label2 = new Label();
+            progressBar = new ProgressBar();
+            lblLoading = new Label();
+            btnStop = new Button();
+            btnDownload = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.FixedPanel = FixedPanel.Panel1;
+            splitContainer.Location = new Point(0, 0);
+            splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.Controls.Add(label1);
+            splitContainer.Panel1.Controls.Add(lstGames);
+            splitContainer.Panel1MinSize = 200;
+            // 
+            // splitContainer.Panel2
+            // 
+            splitContainer.Panel2.Controls.Add(label6);
+            splitContainer.Panel2.Controls.Add(txtNewGameId);
+            splitContainer.Panel2.Controls.Add(label5);
+            splitContainer.Panel2.Controls.Add(txtNewGameName);
+            splitContainer.Panel2.Controls.Add(btnAddGame);
+            splitContainer.Panel2.Controls.Add(label4);
+            splitContainer.Panel2.Controls.Add(txtModIds);
+            splitContainer.Panel2.Controls.Add(label3);
+            splitContainer.Panel2.Controls.Add(txtWorkshop);
+            splitContainer.Panel2.Controls.Add(btnBrowseSteamCMD);
+            splitContainer.Panel2.Controls.Add(txtSteamCMD);
+            splitContainer.Panel2.Controls.Add(label2);
+            splitContainer.Panel2.Controls.Add(progressBar);
+            splitContainer.Panel2.Controls.Add(lblLoading);
+            splitContainer.Panel2.Controls.Add(btnStop);
+            splitContainer.Panel2.Controls.Add(btnDownload);
+            splitContainer.Size = new Size(784, 461);
+            splitContainer.SplitterDistance = 200;
+            splitContainer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Game Names:";
+            // 
+            // lstGames
+            // 
+            lstGames.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lstGames.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lstGames.FormattingEnabled = true;
+            lstGames.ItemHeight = 17;
+            lstGames.Location = new Point(12, 33);
+            lstGames.Name = "lstGames";
+            lstGames.Size = new Size(176, 412);
+            lstGames.TabIndex = 0;
+            lstGames.SelectedIndexChanged += lstGames_SelectedIndexChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(13, 249);
+            label6.Name = "label6";
+            label6.Size = new Size(119, 21);
+            label6.TabIndex = 15;
+            label6.Text = "Enter Game ID:";
+            // 
+            // txtNewGameId
+            // 
+            txtNewGameId.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txtNewGameId.Location = new Point(13, 273);
+            txtNewGameId.Name = "txtNewGameId";
+            txtNewGameId.Size = new Size(546, 25);
+            txtNewGameId.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(13, 193);
+            label5.Name = "label5";
+            label5.Size = new Size(147, 21);
+            label5.TabIndex = 13;
+            label5.Text = "Enter Game Name:";
+            // 
+            // txtNewGameName
+            // 
+            txtNewGameName.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txtNewGameName.Location = new Point(13, 217);
+            txtNewGameName.Name = "txtNewGameName";
+            txtNewGameName.Size = new Size(546, 25);
+            txtNewGameName.TabIndex = 12;
+            // 
+            // btnAddGame
+            // 
+            btnAddGame.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAddGame.Location = new Point(13, 304);
+            btnAddGame.Name = "btnAddGame";
+            btnAddGame.Size = new Size(546, 30);
+            btnAddGame.TabIndex = 11;
+            btnAddGame.Text = "Add Game";
+            btnAddGame.UseVisualStyleBackColor = true;
+            btnAddGame.Click += btnAddGame_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(13, 137);
+            label4.Name = "label4";
+            label4.Size = new Size(195, 21);
+            label4.TabIndex = 10;
+            label4.Text = "Mod IDs (comma separated):";
+            // 
+            // txtModIds
+            // 
+            txtModIds.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txtModIds.Location = new Point(13, 161);
+            txtModIds.Name = "txtModIds";
+            txtModIds.Size = new Size(546, 25);
+            txtModIds.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(13, 81);
+            label3.Name = "label3";
+            label3.Size = new Size(174, 21);
+            label3.TabIndex = 8;
+            label3.Text = "Workshop Content Folder:";
+            // 
+            // txtWorkshop
+            // 
+            txtWorkshop.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txtWorkshop.Location = new Point(13, 105);
+            txtWorkshop.Name = "txtWorkshop";
+            txtWorkshop.ReadOnly = true;
+            txtWorkshop.Size = new Size(546, 25);
+            txtWorkshop.TabIndex = 7;
+            // 
+            // btnBrowseSteamCMD
+            // 
+            btnBrowseSteamCMD.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnBrowseSteamCMD.Location = new Point(519, 49);
+            btnBrowseSteamCMD.Name = "btnBrowseSteamCMD";
+            btnBrowseSteamCMD.Size = new Size(40, 25);
+            btnBrowseSteamCMD.TabIndex = 6;
+            btnBrowseSteamCMD.Text = "📁";
+            btnBrowseSteamCMD.UseVisualStyleBackColor = true;
+            btnBrowseSteamCMD.Click += btnBrowseSteamCMD_Click;
+            // 
+            // txtSteamCMD
+            // 
+            txtSteamCMD.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSteamCMD.Location = new Point(13, 49);
+            txtSteamCMD.Name = "txtSteamCMD";
+            txtSteamCMD.ReadOnly = true;
+            txtSteamCMD.Size = new Size(500, 25);
+            txtSteamCMD.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(13, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 21);
+            label2.TabIndex = 4;
+            label2.Text = "SteamCMD Directory:";
+            // 
+            // progressBar
+            // 
+            progressBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar.Location = new Point(13, 391);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(546, 23);
+            progressBar.TabIndex = 3;
+            // 
+            // lblLoading
+            // 
+            lblLoading.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lblLoading.AutoSize = true;
+            lblLoading.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblLoading.Location = new Point(13, 424);
+            lblLoading.Name = "lblLoading";
+            lblLoading.Size = new Size(126, 21);
+            lblLoading.TabIndex = 2;
+            lblLoading.Text = "⌛ Downloading...";
+            lblLoading.Visible = false;
+            // 
+            // btnStop
+            // 
+            btnStop.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnStop.Enabled = false;
+            btnStop.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnStop.Location = new Point(459, 352);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(100, 30);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnDownload
+            // 
+            btnDownload.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnDownload.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDownload.Location = new Point(353, 352);
+            btnDownload.Name = "btnDownload";
+            btnDownload.Size = new Size(100, 30);
+            btnDownload.TabIndex = 0;
+            btnDownload.Text = "Download";
+            btnDownload.UseVisualStyleBackColor = true;
+            btnDownload.Click += btnDownload_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 461);
+            Controls.Add(splitContainer);
+            MinimumSize = new Size(800, 500);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Mod Downloader";
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel1.PerformLayout();
+            splitContainer.Panel2.ResumeLayout(false);
+            splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer;
+        private Label label1;
+        private ListBox lstGames;
+        private Label label6;
+        private TextBox txtNewGameId;
+        private Label label5;
+        private TextBox txtNewGameName;
+        private Button btnAddGame;
+        private Label label4;
+        private TextBox txtModIds;
+        private Label label3;
+        private TextBox txtWorkshop;
+        private Button btnBrowseSteamCMD;
+        private TextBox txtSteamCMD;
+        private Label label2;
+        private ProgressBar progressBar;
+        private Label lblLoading;
+        private Button btnStop;
+        private Button btnDownload;
+    }
+} 
---
+++ MainForm.cs
@@ -0,0 +1,353 @@
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace ModDownloader
+{
+    public partial class MainForm : Form
+    {
+        private const string CONFIG_FILE = "config.json";
+        private Config _config;
+        private bool _isDownloading;
+        private CancellationTokenSource? _cancellationTokenSource;
+        private System.Windows.Forms.Timer _animationTimer;
+        private int _currentFrame;
+        private readonly string[] _loadingFrames = new[] { "⌛", "⏳" };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadConfig();
+            SetupAnimationTimer();
+            UpdateGameList();
+        }
+
+        private void SetupAnimationTimer()
+        {
+            _animationTimer = new System.Windows.Forms.Timer();
+            _animationTimer.Interval = 500; // 500ms interval
+            _animationTimer.Tick += AnimationTimer_Tick;
+        }
+
+        private void AnimationTimer_Tick(object? sender, EventArgs e)
+        {
+            if (_isDownloading)
+            {
+                _currentFrame = (_currentFrame + 1) % _loadingFrames.Length;
+                lblLoading.Text = $"{_loadingFrames[_currentFrame]} Downloading...";
+            }
+        }
+
+        private void LoadConfig()
+        {
+            try
+            {
+                if (File.Exists(CONFIG_FILE))
+                {
+                    string json = File.ReadAllText(CONFIG_FILE);
+                    _config = JsonConvert.DeserializeObject<Config>(json) ?? new Config();
+                    
+                    // Clean empty games
+                    _config.Games = _config.Games.Where(g => 
+                        !string.IsNullOrWhiteSpace(g.Id) && 
+                        !string.IsNullOrWhiteSpace(g.Name)
+                    ).ToList();
+                }
+                else
+                {
+                    _config = new Config();
+                }
+
+                // Update UI with config values
+                txtSteamCMD.Text = _config.SteamCmdPath;
+                txtWorkshop.Text = _config.WorkshopPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading config: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _config = new Config();
+            }
+        }
+
+        private void SaveConfig()
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(_config, Formatting.Indented);
+                File.WriteAllText(CONFIG_FILE, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving config: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateGameList()
+        {
+            lstGames.Items.Clear();
+            foreach (var game in _config.Games)
+            {
+                lstGames.Items.Add(game.Name);
+            }
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (_isDownloading)
+                return;
+
+            if (string.IsNullOrWhiteSpace(txtModIds.Text))
+            {
+                MessageBox.Show("Please enter mod IDs", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] modIds = txtModIds.Text.Split(',')
+                                        .Select(id => id.Trim())
+                                        .Where(id => !string.IsNullOrWhiteSpace(id))
+                                        .ToArray();
+
+            if (!modIds.Any())
+            {
+                MessageBox.Show("No valid mod IDs found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _isDownloading = true;
+            _cancellationTokenSource = new CancellationTokenSource();
+            btnDownload.Enabled = false;
+            btnStop.Enabled = true;
+            progressBar.Value = 0;
+            lblLoading.Visible = true;
+            _animationTimer.Start();
+
+            try
+            {
+                string? lastSuccessfulFolder = null;
+                int totalMods = modIds.Length;
+
+                for (int i = 0; i < modIds.Length; i++)
+                {
+                    if (_cancellationTokenSource.Token.IsCancellationRequested)
+                        break;
+
+                    string modId = modIds[i];
+                    var progress = new Progress<int>(p =>
+                    {
+                        int overallProgress = (int)((i * 100 + p) / (float)totalMods);
+                        progressBar.Value = Math.Min(100, Math.Max(0, overallProgress));
+                    });
+
+                    var result = await DownloadMod(modId, _config.CurrentGameId, progress, _cancellationTokenSource.Token);
+                    if (result.Success)
+                    {
+                        lastSuccessfulFolder = result.ModsFolder;
+                    }
+                }
+
+                if (lastSuccessfulFolder != null && Directory.Exists(lastSuccessfulFolder))
+                {
+                    Process.Start("explorer.exe", lastSuccessfulFolder);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Download cancelled", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during download: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _isDownloading = false;
+                btnDownload.Enabled = true;
+                btnStop.Enabled = false;
+                lblLoading.Visible = false;
+                _animationTimer.Stop();
+                _cancellationTokenSource?.Dispose();
+                _cancellationTokenSource = null;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+        }
+
+        private void btnBrowseSteamCMD_Click(object sender, EventArgs e)
+        {
+            using var dialog = new FolderBrowserDialog
+            {
+                Description = "Select SteamCMD Directory",
+                UseDescriptionForTitle = true
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _config.SteamCmdPath = dialog.SelectedPath;
+                txtSteamCMD.Text = dialog.SelectedPath;
+
+                // Set workshop path automatically
+                string workshopPath = Path.Combine(dialog.SelectedPath, "steamapps", "workshop", "content");
+                Directory.CreateDirectory(workshopPath);
+                _config.WorkshopPath = workshopPath;
+                txtWorkshop.Text = workshopPath;
+
+                SaveConfig();
+            }
+        }
+
+        private void btnAddGame_Click(object sender, EventArgs e)
+        {
+            string gameName = txtNewGameName.Text.Trim();
+            string gameId = txtNewGameId.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(gameName) || string.IsNullOrWhiteSpace(gameId))
+            {
+                MessageBox.Show("Game Name and Game ID cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (_config.Games.Any(g => g.Id == gameId))
+            {
+                MessageBox.Show("A game with this ID already exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _config.Games.Add(new Game { Id = gameId, Name = gameName });
+            SaveConfig();
+            UpdateGameList();
+
+            txtNewGameName.Clear();
+            txtNewGameId.Clear();
+        }
+
+        private void lstGames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstGames.SelectedIndex >= 0)
+            {
+                string selectedName = lstGames.SelectedItem.ToString() ?? "";
+                var game = _config.Games.FirstOrDefault(g => g.Name == selectedName);
+                if (game != null)
+                {
+                    _config.CurrentGameId = game.Id;
+                }
+            }
+        }
+
+        private async Task<(bool Success, string? ModsFolder)> DownloadMod(string modId, string gameId, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            string exeDir = AppDomain.CurrentDomain.BaseDirectory;
+            string tempDir = Path.Combine(exeDir, "temp");
+            Directory.CreateDirectory(tempDir);
+
+            try
+            {
+                string steamCmdPath = Path.Combine(_config.SteamCmdPath, "steamcmd.exe");
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = steamCmdPath,
+                    Arguments = $"+login anonymous +workshop_download_item {gameId} {modId} +quit",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = _config.SteamCmdPath
+                };
+
+                using var process = new Process { StartInfo = startInfo };
+                process.Start();
+
+                bool downloading = false;
+                int currentProgress = 0;
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        process.Kill();
+                        throw new OperationCanceledException();
+                    }
+
+                    string? line = await process.StandardOutput.ReadLineAsync();
+                    if (line == null) continue;
+
+                    if (line.Contains("Downloading item"))
+                    {
+                        downloading = true;
+                        progress.Report(10);
+                    }
+                    else if (line.Contains("Download Complete"))
+                    {
+                        progress.Report(90);
+                    }
+                    else if (downloading && line.Contains("Success"))
+                    {
+                        progress.Report(100);
+                    }
+                    else if (downloading)
+                    {
+                        currentProgress = Math.Min(currentProgress + 2, 85);
+                        progress.Report(currentProgress);
+                    }
+
+                    await Task.Delay(100, cancellationToken);
+                }
+
+                await process.WaitForExitAsync(cancellationToken);
+
+                if (process.ExitCode == 0)
+                {
+                    string modPath = Path.Combine(_config.WorkshopPath, gameId, modId);
+                    if (Directory.Exists(modPath))
+                    {
+                        string modsFolder = Path.Combine(exeDir, "mods");
+                        Directory.CreateDirectory(modsFolder);
+
+                        foreach (string file in Directory.GetFiles(modPath))
+                        {
+                            string fileName = Path.GetFileName(file);
+                            if (fileName != "preview.png" && fileName != "workshop_data.json")
+                            {
+                                File.Copy(file, Path.Combine(modsFolder, fileName), true);
+                            }
+                        }
+
+                        Directory.Delete(modPath, true);
+                        return (true, modsFolder);
+                    }
+                }
+
+                return (false, null);
+            }
+            finally
+            {
+                if (Directory.Exists(tempDir))
+                {
+                    Directory.Delete(tempDir, true);
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+            base.OnFormClosing(e);
+        }
+    }
+
+    public class Config
+    {
+        public List<Game> Games { get; set; } = new();
+        public string SteamCmdPath { get; set; } = "";
+        public string WorkshopPath { get; set; } = "";
+        public string CurrentGameId { get; set; } = "";
+    }
+
+    public class Game
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+} 
